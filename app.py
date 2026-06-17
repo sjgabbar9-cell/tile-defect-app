80dd87368056747daa4de985cb4fca73faff2f32
sjgabbar9-cell <sjgabbar9@gmail.com>
2026-06-17 12:37:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -324,13 +324,13 @@ elif st.session_state.page == "history":
         col3.write("Size:", selected_row.get("Size", ""))
 
         st.write("Surface:", selected_row.get("Surface", ""))
-        # ✅ Defect Details
+       # ✅ Defect Details
         st.subheader("Defect Details")
 
         filtered_df = df[
-            (df["Date"] == selected_row["Date"]) &
-            (df["Batch"] == selected_row["Batch"]) &
-            (df["Item"] == selected_row["Item"])
+            (df["Date"] == str(selected_row["Date"])) &
+            (df["Batch"] == str(selected_row["Batch"])) &
+            (df["Item"] == str(selected_row["Item"]))
         ]
         if filtered_df.empty:
             st.warning("No matching defect data found")
