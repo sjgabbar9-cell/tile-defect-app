26d394ede21ec1fec67889f49b51962839220154
sjgabbar9-cell <sjgabbar9@gmail.com>
2026-06-17 14:26:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -363,7 +363,7 @@ elif st.session_state.page == "history":
        # cumulative %
     defect_data["Cum %"] = defect_data["Qty"].cumsum() / defect_data["Qty"].sum() * 100
 
-    fig, ax1 = plt.subplots(figsize=(2,1))  # ✅ SMALL SIZE
+    fig, ax1 = plt.subplots(figsize=(5,3))  # ✅ SMALL SIZE
 
        # bar chart
     ax1.bar(defect_data["Defect"], defect_data["Qty"])
@@ -390,7 +390,7 @@ elif st.session_state.page == "history":
 
     dept_data["Cum %"] = dept_data["Qty"].cumsum() / dept_data["Qty"].sum() * 100
 
-    fig2, ax1 = plt.subplots(figsize=(2,1))  # ✅ SMALL SIZE
+    fig2, ax1 = plt.subplots(figsize=(5,3))  # ✅ SMALL SIZE
 
 # bar
     ax1.bar(dept_data["Department"], dept_data["Qty"])
