0d7827d6a637d1d1862e203707dcdbc034d6da33
sjgabbar9-cell <sjgabbar9@gmail.com>
2026-06-17 12:33:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -298,7 +298,7 @@ elif st.session_state.page == "history":
 
         # ✅ Defect %
         summary["Defect %"] = (
-            summary["Date"] = summary["Date"].astype(str)
+            summary["Date"] == summary["Date"].astype(str)
             summary["Defective Tiles"] / summary["Total Tiles"] * 100
         ).round(2)
         
